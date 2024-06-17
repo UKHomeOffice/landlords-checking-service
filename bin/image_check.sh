862c3439b9916b1f4b7cdcbdb6dcac145ed9b2b9
Gador Cruz <gador.cruz@digital.homeoffice.gov.uk>
2024-06-17 13:59:04 +0100

    TLF-74-V2: add snyk config set api=SNYK_TOKEN && snyk testsnyk dependancies and notify stub in drone.yaml yarn run build and remove build

--- bin/image_check.sh
+++ bin/image_check.sh
@@ -3,4 +3,3 @@
 docker build -t test-repo-image:latest . || docker build -t test-repo-image:latest ..
 snyk config set api=$SNYK_TOKEN
 snyk container test test-repo-image
-curl -s https://ci-tools.anchore.io/inline_scan-latest | bash -s -- -r test-repo-image:latest
