88b22602193129eeea8be6bffb5c59f574332461
dk4g <157156245+dk4g@users.noreply.github.com>
2024-06-24 08:44:14 +0100

    Initial questions about tenant;

--- apps/lcs/sections/summary-data-sections.js
+++ apps/lcs/sections/summary-data-sections.js
@@ -1,9 +1,22 @@
 
 'use strict';
 
+const moment = require('moment');
+const PRETTY_DATE_FORMAT = 'DD MMMM YYYY';
+
 module.exports = {
   'tenants-information': {
-    steps: []
+    steps: [
+      {
+        step: '/property-occupied',
+        field: 'person-live-in'
+      },
+      {
+        step: '/property-occupied',
+        field: 'when-person-moved-in',
+        parse: d => d && moment(d).format(PRETTY_DATE_FORMAT)
+      }
+    ]
   },
   'tenants-contact-details': {
     steps: []
