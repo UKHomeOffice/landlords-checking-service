6b31f28741eb2d49263acb7b768d0ea87917b4bf
Mike Maratheftis <mike.maratheftis@digital.homeoffice.gov.uk>
2024-07-29 16:22:05 +0100

    Fixed bug where postcode was still validating when not appearing

--- apps/lcs/behaviours/saving-details.js
+++ apps/lcs/behaviours/saving-details.js
@@ -1,15 +1,11 @@
 module.exports = superclass => class extends superclass {
   configure(req, res, next) {
     const isTenantLivedInProperty = req.sessionModel.get('person-live-in');
-    // console.log(isTenantLivedInProperty);
     const isCurrentTenant = isTenantLivedInProperty === 'yes' ? true : false;
     req.sessionModel.set('isCurrentTenant', isCurrentTenant);
-    // console.log(req.sessionModel);
-
     if (!isCurrentTenant) {
       req.form.options.fields['landlord-or-agent-postcode'] = {};
     }
-    console.log(req.form.options);
     return super.configure(req, res, next);
   }
 };
