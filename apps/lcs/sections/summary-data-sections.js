60fffdc6cdb06157cf09385a0d752375ca0df001
Mike Maratheftis <160880141+MikeMaratheftis@users.noreply.github.com>
2024-08-22 13:50:06 +0100

    LCS - Summary page (#40)
    * LCS summary page
    
    ---------
    
    Co-authored-by: Mike Maratheftis <mike.maratheftis@digital.homeoffice.gov.uk>

--- apps/lcs/sections/summary-data-sections.js
+++ apps/lcs/sections/summary-data-sections.js
@@ -14,7 +14,49 @@ module.exports = {
       {
         step: '/property-occupied',
         field: 'when-person-moved-in',
+        parse: (value, req) => {
+          if(req.sessionModel.get('person-live-in') === 'no') {
+            return null;
+          }
+          return value && moment(value).format(PRETTY_DATE_FORMAT);
+        }
+      },
+      {
+        step: '/tenant-details',
+        field: 'ho-ref-number'
+      },
+      {
+        step: '/tenant-details',
+        field: 'tenant-full-name'
+      },
+      {
+        step: '/tenant-details',
+        field: 'tenant-dob',
         parse: d => d && moment(d).format(PRETTY_DATE_FORMAT)
+      },
+      {
+        step: '/tenant-details',
+        field: 'tenant-nationality'
+      },
+      {
+        step: '/tenant-address',
+        field: 'tenant-address-line-1'
+      },
+      {
+        step: '/tenant-address',
+        field: 'tenant-address-line-2'
+      },
+      {
+        step: '/tenant-address',
+        field: 'tenant-town-or-city'
+      },
+      {
+        step: '/tenant-address',
+        field: 'tenant-county'
+      },
+      {
+        step: '/tenant-address',
+        field: 'tenant-postcode'
       }
     ]
   },
@@ -44,6 +86,46 @@ module.exports = {
     ]
   },
   'landlord-agent-information': {
-    steps: []
+    steps: [
+      {
+        step: '/landlord-information',
+        field: 'landlord-or-agent-name'
+      },
+      {
+        step: '/landlord-information',
+        field: 'landlord-or-agent-company'
+      },
+      {
+        step: '/landlord-information',
+        field: 'landlord-or-agent-email'
+      },
+      {
+        step: '/landlord-information',
+        field: 'landlord-or-agent-tel'
+      },
+      {
+        step: 'tenant-address',
+        field: 'tenant-address-details',
+        parse: (list, req) => {
+          if (!req.sessionModel.get('steps').includes('/tenant-address')) {
+            return null;
+          }
+          let addressDetails = [
+            req.sessionModel.get('tenant-address-line-1'),
+            req.sessionModel.get('tenant-address-line-2'),
+            req.sessionModel.get('tenant-town-or-city'),
+            req.sessionModel.get('tenant-county'),
+            req.sessionModel.get('tenant-postcode')
+          ];
+          addressDetails = addressDetails.filter(line => line);
+          req.sessionModel.set('tenantAddressDetails', addressDetails.join(', '));
+          return addressDetails.join('\n');
+        }
+      },
+      {
+        step: '/tenant-address',
+        field: '/tenant-address'
+      }
+    ]
   }
 };
