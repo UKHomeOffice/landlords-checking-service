ece5e2ab6bf3579e8a61319a0a19c8981a38a557
dk4g <157156245+dk4g@users.noreply.github.com>
2024-08-22 18:46:42 +0100

    fixed linting;

--- apps/lcs/behaviours/locals-enricher.js
+++ apps/lcs/behaviours/locals-enricher.js
@@ -1,13 +1,13 @@
 module.exports = superclass => class extends superclass {
-    getErrorMessage(error, req, res) {
-        const locals = {};
-        const valuesEnriched = req.sessionModel.get('valuesEnriched');
-        
-        if (valuesEnriched && Object.keys(valuesEnriched).length > 0) {
-            locals.valuesEnriched = valuesEnriched;
+  getErrorMessage(error, req, res) {
+    const locals = {};
+    const valuesEnriched = req.sessionModel.get('valuesEnriched');
 
-            Object.assign(res.locals, locals);
-        }
-        return super.getErrorMessage(error, req, res);
+    if (valuesEnriched && Object.keys(valuesEnriched).length > 0) {
+      locals.valuesEnriched = valuesEnriched;
+
+      Object.assign(res.locals, locals);
     }
+    return super.getErrorMessage(error, req, res);
+  }
 };
