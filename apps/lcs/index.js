5311cf9ab3436a8735ed059fa1f4da0311d04aad
dk4g <157156245+dk4g@users.noreply.github.com>
2024-09-02 16:07:02 +0100

    updated condition for forking route;

--- apps/lcs/index.js
+++ apps/lcs/index.js
@@ -9,6 +9,24 @@ const customRedirect = require('./behaviours/custom-redirect');
 const valuesEnricher = require('./behaviours/values-enricher')(config);
 const localsEnricher  = require('./behaviours/locals-enricher');
 
+/**
+ * Checks if the user should be redirected to the '/before-1988' page based on the tenant's date of birth.
+ *
+ * @param {string} tenantDob - The tenant's date of birth in ISO format.
+ * @param {string} startOf1988 - The start date of 1988 in ISO format.
+ * @returns {boolean} - Returns true if the tenant's DOB is before the start of 1988 and not equal to the excluded date.
+ */
+function shouldRedirectToBefore1988(tenantDob, startOf1988) {
+  const excludedDate = '1987-12-30';
+
+  /**
+   * If the tenant's date of birth is before the cutoff date and not equal to 1987-12-30,
+   * then redirect to '/before-1988'. This allows tenants born on 1987-12-30 to enter
+   * 1987-12-31 as their date of entry to the UK on the '/extra-tenant-details' page and progress.
+   */
+  return tenantDob < startOf1988 && tenantDob !== excludedDate;
+}
+
 const steps =  {
   '/start': {
     next: '/property-occupied'
@@ -49,7 +67,7 @@ const steps =  {
     forks: [
       {
         target: '/before-1988',
-        condition: req => req.sessionModel.get('tenant-dob') < config.startOf1988
+        condition: req => shouldRedirectToBefore1988(req.sessionModel.get('tenant-dob'), config.startOf1988)
       }
     ]
   },
