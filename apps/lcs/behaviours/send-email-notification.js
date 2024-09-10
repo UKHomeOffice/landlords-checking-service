90a80352804540341adb05679d10bf09245bc59a
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2024-09-10 12:02:19 +0100

    TLF-117 tidy conditional yes/no in personalisation

--- apps/lcs/behaviours/send-email-notification.js
+++ apps/lcs/behaviours/send-email-notification.js
@@ -16,7 +16,7 @@ module.exports = class SendEmailConfirmation {
       is_tenant: req.sessionModel.get('isCurrentTenant') ?? '',
       is_prospective_tenant: !req.sessionModel.get('isCurrentTenant') ?? '',
       existing_occupier: getLabel('person-live-in', req.sessionModel.get('person-live-in')),
-      is_existing_occupier: req.sessionModel.get('person-live-in') === 'yes' ? 'yes' : 'no',
+      is_existing_occupier: req.sessionModel.get('person-live-in'),
       person_moved_into_property: req.sessionModel.get('when-person-moved-in') ?
         moment(req.sessionModel.get('when-person-moved-in')).format(config.PRETTY_DATE_FORMAT) : '',
       ho_ref_number: req.sessionModel.get('ho-ref-number'),
@@ -24,8 +24,8 @@ module.exports = class SendEmailConfirmation {
       date_of_birth: moment(req.sessionModel.get('tenant-dob')).format(config.PRETTY_DATE_FORMAT),
       country_of_nationality: req.sessionModel.get('tenant-nationality'),
       full_address: req.sessionModel.get('tenantAddress').join(', '),
-      is_before_1988: req.sessionModel.get('steps').includes('/before-1988') &&
-        req.sessionModel.get('in-uk-before-1988') === 'yes' ? 'yes' : 'no',
+      is_before_1988: req.sessionModel.get('steps').includes('/before-1988') ?
+        req.sessionModel.get('before-or-after-1988') : 'no',
       date_of_entry: req.sessionModel.get('date-tenant-moved-uk') ?
         moment(req.sessionModel.get('date-tenant-moved-uk')).format(config.PRETTY_DATE_FORMAT) : '',
       place_of_birth: req.sessionModel.get('extra-tenant-pob') ?? '',
